2385db18de86387803dafdc0608ba2cbd06f50ec
steroman <stefanoromanelli86@gmail.com>
2020-11-02 17:49:57 +0100

    sort sections alphabetically

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,17 +18,17 @@ module.exports = {
         'alt-text',
         'accessibility-terms',
         'admonitions',
-        'use-simple-words-concise-sentences',
-        'pronouns-and-gender',
         'bias-free',
-        'periods',
-        'we',
         'chapter-and-section',
-        'headings',
-        'text-style-formatting',
         'code-samples',
+        'headings',
         'numbers',
-        'screenshots'
+        'use-simple-words-concise-sentences',
+        'periods',
+        'pronouns-and-gender',
+        'screenshots',
+        'text-style-formatting',
+        'we',
       ]
     }
   ]
