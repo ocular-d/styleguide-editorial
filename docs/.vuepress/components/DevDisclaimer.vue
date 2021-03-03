26abe9ef6bdb404b9b0311cd48e3f940e57ed507
sven <sven@so36.net>
2021-03-03 16:55:24 +0100

    docs(cli) start with cli guide

---
+++ docs/.vuepress/components/DevDisclaimer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content deprecation-warning">
+    <p>
+      <strong>⚠️ Beta Version</strong>
+      This documentation is in development and subject to change!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeprecationWarning"
+};
+</script>
+
+<style lang="stylus" scoped>
+.deprecation-warning {
+  $bg = #ed0100;
+  padding: 0 2rem;
+  border: 1px solid $bg;
+  background-color: #ffe5e5;
+  //background-color: rgba($bg, 20%);
+  border-radius: 4px;
+}
+</style>
